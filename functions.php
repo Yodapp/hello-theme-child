f0c6052e4b3012d13c43e9b911e4f8fc29e0b400
Daniel Nordmark <36842813+Yodapp@users.noreply.github.com>
2019-12-08 16:26:49 +0100

    More flexible way to load stylesheets

--- functions.php
+++ functions.php
@@ -15,13 +15,16 @@ define( 'DJUPESKOG_HELLO_CHILD_THEME', '1.0.0' );
  * Load child theme css and optional scripts
  */
 function djupeskog_hello_elementor_child_enqueue_scripts() {
+	
+	// Load Hello theme style first
+    	$parent_style = 'parent-style'; 
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	// Load Djupeskog style
 	wp_enqueue_style(
 		'djupeskog_hello-elementor-child-style',
 		get_stylesheet_directory_uri() . '/style.css',
-		[
-			'djupeskog_hello-elementor-theme-style',
-		],
-		'1.0.0'
+		array( $parent_style ),
+		wp_get_theme()->get('Version')
 	);
 }
 add_action( 'wp_enqueue_scripts', 'djupeskog_hello_elementor_child_enqueue_scripts' );
